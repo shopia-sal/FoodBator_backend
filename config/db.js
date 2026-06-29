7e23886090cde541c786f950b91488aa828201fb
Shopia Salma Siregar <shopiasalma599@gmail.com>
2026-06-29 16:26:36 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,18 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://bator_db_user:FoodBator123@cluster0.klikxg2.mongodb.net/FoodWebBator')
-    .then(() => console.log('DB CONNECTED'))
-}
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    isConnected = true;
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Error:", err);
+    throw err;
+  }
+};
+
+export default connectDB;
